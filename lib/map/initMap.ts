3455b4daab09b4f1cafb06d44428c0ce456d8c08
kartikv8 <kartik@flywheel.com>
2026-04-01 11:51:42 +0530

    Migrate basemap to token-free OSM raster style

--- lib/map/initMap.ts
+++ lib/map/initMap.ts
@@ -1,86 +1,40 @@
-import maplibregl, { LngLatLike, LngLatBounds, Map, NavigationControl } from "maplibre-gl";
-import { DEFAULT_MAP_VIEW, MapStyleKey, getMapStyleUrl } from "./styles";
+import maplibregl, {
+  AttributionControl,
+  LngLatLike,
+  LngLatBounds,
+  Map,
+  NavigationControl,
+} from "maplibre-gl";
+import { DEFAULT_MAP_VIEW, OSM_RASTER_STYLE } from "./styles";
 
 export type InitMapParams = {
   container: HTMLDivElement;
-  styleKey: MapStyleKey;
   onCursorMove?: (coords: { lng: number; lat: number }) => void;
 };
 
 export type InitMapResult = {
   map: Map;
-  setMapStyle: (styleKey: MapStyleKey) => Promise<void>;
   resetView: () => void;
   cleanup: () => void;
 };
 
-function waitForStyleLoad(map: Map): Promise<void> {
-  return new Promise((resolve) => {
-    if (map.isStyleLoaded()) {
-      resolve();
-      return;
-    }
-
-    const handleStyleData = () => {
-      if (!map.isStyleLoaded()) {
-        return;
-      }
-      map.off("styledata", handleStyleData);
-      resolve();
-    };
-
-    map.on("styledata", handleStyleData);
-  });
-}
-
-function getTransformRequest() {
-  const token = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-
-  return (url: string, resourceType?: string) => {
-    if (!token || !url.startsWith("mapbox://")) {
-      return { url };
-    }
-
-    if (url.startsWith("mapbox://sprites/")) {
-      const path = url.replace("mapbox://sprites/", "");
-      return { url: `https://api.mapbox.com/styles/v1/${path}?access_token=${token}` };
-    }
-
-    if (url.startsWith("mapbox://fonts/")) {
-      const path = url.replace("mapbox://fonts/", "");
-      return { url: `https://api.mapbox.com/fonts/v1/${path}?access_token=${token}` };
-    }
-
-    if (url.startsWith("mapbox://")) {
-      const tilesetId = url.replace("mapbox://", "");
-      return { url: `https://api.mapbox.com/v4/${tilesetId}.json?secure&access_token=${token}` };
-    }
-
-    return { url, resourceType };
-  };
-}
-
-export function initMap({ container, styleKey, onCursorMove }: InitMapParams): InitMapResult {
+export function initMap({ container, onCursorMove }: InitMapParams): InitMapResult {
   const map = new maplibregl.Map({
     container,
-    style: getMapStyleUrl(styleKey),
+    style: OSM_RASTER_STYLE,
     center: DEFAULT_MAP_VIEW.center as LngLatLike,
     zoom: DEFAULT_MAP_VIEW.zoom,
-    transformRequest: getTransformRequest(),
+    attributionControl: false,
   });
 
   map.addControl(new NavigationControl({ showZoom: true, showCompass: true }), "top-right");
+  map.addControl(new AttributionControl({ compact: false }), "bottom-right");
 
   const handleMouseMove = (event: { lngLat: { lng: number; lat: number } }) => {
     onCursorMove?.({ lng: event.lngLat.lng, lat: event.lngLat.lat });
   };
 
   map.on("mousemove", handleMouseMove);
-
-  const setMapStyle = async (nextStyleKey: MapStyleKey) => {
-    map.setStyle(getMapStyleUrl(nextStyleKey));
-    await waitForStyleLoad(map);
-  };
 
   const resetView = () => {
     map.easeTo({ center: DEFAULT_MAP_VIEW.center as LngLatLike, zoom: DEFAULT_MAP_VIEW.zoom, duration: 500 });
@@ -91,7 +45,7 @@ export function initMap({ container, styleKey, onCursorMove }: InitMapParams): I
     map.remove();
   };
 
-  return { map, setMapStyle, resetView, cleanup };
+  return { map, resetView, cleanup };
 }
 
 export function fitMapToMultiPolygon(map: Map, geometry: GeoJSON.MultiPolygon) {
